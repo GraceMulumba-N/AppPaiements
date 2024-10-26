2a03a1960b8d1a9d530d8be91c4e69889e7e09eb
Grace_Mulumba <grace.mulumbandaya@monboreal.ca>
2024-10-26 18:38:52 -0400

    Ajout de classe Utilisateur

---
+++ AppPaiements/Utilisateur.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class Utilisateur
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public List<Paiement> Paiements { get; set; }
+
+        public Utilisateur()
+        {
+            Paiements = new List<Paiement>();
+        }
+    }
+
+}
