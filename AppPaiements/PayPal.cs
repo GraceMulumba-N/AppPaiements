213a2ddc9a36b949af1e77ef0715432fc09b7373
Grace_Mulumba <grace.mulumbandaya@monboreal.ca>
2024-10-26 18:34:09 -0400

    Ajout de la classe paypal

---
+++ AppPaiements/PayPal.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    public class PayPal : Paiement
+    {
+        public string Email { get; set; }
+        public double Montant { get; set; }
+        public override void EffectuerPaiement()
+        {
+            Console.WriteLine($"Paiement de {Montant} effectué par {Email}");
+        }
+    }
+
+}
--- AppPaiements/Program.cs
+++ AppPaiements/Program.cs
@@ -6,15 +6,31 @@ namespace AppPaiements
     {
         static void Main(string[] args)
         {
-            CarteCredit achat1 = new CarteCredit { Nom = "Grace", Prenom = "Mulumba", Date = DateTime.Now, NumeroCarte = "1234567890123456", Montant = 59.99 };
-            CarteCredit achat2 = new CarteCredit { Nom = "Aurelie", Prenom = "Mulumba", Date = DateTime.Now, NumeroCarte = "9876543210987654", Montant = 102.0 };
-            CarteCredit achat3 = new CarteCredit { Nom = "Olive", Prenom = "Ndaya", Date = DateTime.Now, NumeroCarte = "1357924680135792", Montant = 151.0 };
+            // instance utilisateur
+            Utilisateur utilisateur = new Utilisateur { Nom = "Grace", Prenom = "Mulumba" };
 
-            Console.WriteLine(achat1.ToString());
-            Console.WriteLine(achat2.ToString());
-            Console.WriteLine(achat3.ToString());
+            // instances CarteCredit
+            CarteCredit achat1 = new CarteCredit { Nom = "Aurelie", Prenom = "Mulumba", Date = DateTime.Now, NumeroCarte = "1234567890123456", Montant = 59.99 };
+            CarteCredit achat2 = new CarteCredit { Nom = "Marie", Prenom = "Katehe", Date = DateTime.Now, NumeroCarte = "9876543210987654", Montant = 102.0 };
+            CarteCredit achat3 = new CarteCredit { Nom = "Kesie", Prenom = "Mbutu", Date = DateTime.Now, NumeroCarte = "1357924680135792", Montant = 169.0 };
+
+            // instances PayPal
+            PayPal paiement1 = new PayPal { Nom = "Mike", Prenom = "Ross", Date = DateTime.Now, Email = "mike@gmail.com", Montant = 30.0 };
+            PayPal paiement2 = new PayPal { Nom = "Bob", Prenom = "Sponge", Date = DateTime.Now, Email = "spongebob@example.com", Montant = 75.0 };
+
+            // achats et paiements 
+            utilisateur.Paiements.Add(achat1);
+            utilisateur.Paiements.Add(achat2);
+            utilisateur.Paiements.Add(achat3);
+            utilisateur.Paiements.Add(paiement1);
+            utilisateur.Paiements.Add(paiement2);
+
+            // Afficher les détails de tous les paiements
+            foreach (var paiement in utilisateur.Paiements)
+            {
+                Console.WriteLine(paiement.ToString());
+            }
         }
     }
 
-}
 }
