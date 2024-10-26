4e25bedcf7560e6b3a79c54e2db444134ca405bd
Grace_Mulumba <grace.mulumbandaya@monboreal.ca>
2024-10-26 17:51:33 -0400

    Ajout de la classe Paiement

---
+++ AppPaiements/Paiement.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AppPaiements
+{
+    internal class Paiement
+    {
+        public string Nom { get; set; }
+        public string Prenom { get; set; }
+        public DateTime Date { get; set; }
+        public abstract void EffectuerPaiement();
+       
+
+    }
+}
